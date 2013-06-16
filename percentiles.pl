a8fc0d0bd9e2382f4943eba94e4b82675e90b61e
Mike Adler <mike.adler@huffingtonpost.com>
2013-06-16 16:27:12 -0400

    initial

---
+++ percentiles.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use Scalar::Util qw(looks_like_number);
+
+my @sorted = sort {$a <=> $b} grep {looks_like_number($_)} <STDIN>;
+my $total = scalar @sorted;
+
+foreach my $percentile (qw(50 90 95 99 99.9 99.99)) {
+  my $index = int($total * $percentile / 100);
+  my $value = $sorted[$index];
+  printf "%5.1f%% : %d\n", $percentile, $value;
+}
